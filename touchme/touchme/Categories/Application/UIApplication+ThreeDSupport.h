e87530efe6b3bfb87ed364dcb450cc47346fd88c
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2016-03-08 07:33:59 +0100

    added checking for 3D Touch availabilty for all ViewControllers

---
+++ touchme/touchme/Categories/Application/UIApplication+ThreeDSupport.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+ThreeDSupport.h
+//  touchme
+//
+//  Created by Mihaela Mihaljevic Jakic on 08/03/16.
+//  Copyright © 2016 Mihaela Mihaljevic Jakic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (ThreeDSupport)
+
+@end
---
+++ touchme/touchme/Categories/Application/UIApplication+ThreeDSupport.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+ThreeDSupport.m
+//  touchme
+//
+//  Created by Mihaela Mihaljevic Jakic on 08/03/16.
+//  Copyright © 2016 Mihaela Mihaljevic Jakic. All rights reserved.
+//
+
+#import "UIApplication+ThreeDSupport.h"
+
+@implementation UIApplication (ThreeDSupport)
+
+@end
