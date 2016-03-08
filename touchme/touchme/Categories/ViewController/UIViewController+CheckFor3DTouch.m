c03c00bd815d9573c187177e675ccc966777cd95
Mihaela Mihaljevic Jakic <mihaelamj@me.com>
2016-03-08 07:12:02 +0100

    made better file structure

---
+++ touchme/touchme/Categories/ViewController/UIViewController+CheckFor3DTouch.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+CheckFor3DTouch.m
+//  touchme
+//
+//  Created by Mihaela Mihaljevic Jakic on 08/03/16.
+//  Copyright © 2016 Mihaela Mihaljevic Jakic. All rights reserved.
+//
+
+#import "UIViewController+CheckFor3DTouch.h"
+
+@implementation UIViewController (CheckFor3DTouch)
+
+@end
